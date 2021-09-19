891f6651c18f54cdf78cd22daccda3c27328dc2d
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2021-09-18 22:36:54 -0700

    search

--- odinson/engine.py
+++ odinson/engine.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from .results import OdinsonResults
 
 
@@ -5,7 +6,17 @@ class ExtractorEngine:
     def __init__(self, extractor_engine):
         self.extractor_engine = extractor_engine
 
-    def search(self, pattern: str) -> OdinsonResults:
+    def num_docs(self) -> int:
+        return self.extractor_engine.numDocs()
+
+    def search(
+        self,
+        pattern: str,
+        n: Optional[int] = None,
+        disable_match_selector: bool = False,
+    ) -> OdinsonResults:
+        if n is None:
+            n = self.num_docs()
         query = self.extractor_engine.mkQuery(pattern)
-        res = self.extractor_engine.query(query)
+        res = self.extractor_engine.query(query, n, disable_match_selector)
         return OdinsonResults.from_scala(res)
