bbb834bc8c794532043fff1de826bb9a74f6dc99
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2021-09-26 20:00:12 -0700

    find_jar

--- odinson/gateway.py
+++ odinson/gateway.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+import os
 from typing import Optional
 from py4j.java_gateway import JavaGateway, GatewayParameters, launch_gateway
 from .document import Document
@@ -10,10 +12,16 @@ class OdinsonGateway:
         self.entry_point = self.gateway.jvm.odinson.EntryPoint()
 
     @classmethod
-    def launch_gateway(cls, classpath="", javaopts=[]):
-        # classpath="scala-gateway/target/scala-2.12/scala-gateway-assembly-0.1.0-SNAPSHOT.jar"
+    def launch_gateway(
+        cls, classpath: Optional[str] = None, javaopts: list[str] = []
+    ) -> OdinsonGateway:
+        if classpath is None:
+            classpath = find_jar()
         port, proc = launch_gateway(
-            classpath=classpath, javaopts=javaopts, return_proc=True
+            classpath=classpath,
+            javaopts=javaopts,
+            return_proc=True,
+            die_on_exit=True,
         )
         gateway = JavaGateway(
             gateway_parameters=GatewayParameters(port=port, auto_convert=True),
@@ -32,3 +40,9 @@ class OdinsonGateway:
         data = [d.to_dict() for d in documents]
         ee = self.entry_point.mkMemoryIndex(data)
         return ExtractorEngine(ee)
+
+
+def find_jar():
+    dev_jar = "scala-gateway/target/scala-2.12/odinson-entrypoint.jar"
+    if os.path.exists(dev_jar):
+        return dev_jar
